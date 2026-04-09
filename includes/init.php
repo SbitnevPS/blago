aefe2987abc1f9bcc4661159b0703275baba0adc
SbitnevPS <sbitnev.ps@bk.ru>
2026-04-10 01:58:28 +0300

    Add autoload.php requirement in init.php
    Signed-off-by: SbitnevPS <sbitnev.ps@bk.ru>

--- includes/init.php
+++ includes/init.php
@@ -2,7 +2,7 @@
 // includes/init.php
 
 require_once __DIR__ . '/../config.php';
-
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/mailer.php';
 require_once __DIR__ . '/diplomas.php';
 require_once __DIR__ . '/vk-publications.php';
