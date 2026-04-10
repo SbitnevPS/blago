1da25de05c000589e1815f9a9de264cbec8f6285
SbitnevPS <sbitnev.ps@bk.ru>
2026-04-10 09:56:24 +0300

    Fix optional autoload bootstrap path in init

--- includes/init.php
+++ includes/init.php
@@ -2,7 +2,19 @@
 // includes/init.php
 
 require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/vendor/autoload.php';
+
+$autoloadCandidates = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/vendor/autoload.php',
+];
+
+foreach ($autoloadCandidates as $autoloadPath) {
+    if (is_file($autoloadPath)) {
+        require_once $autoloadPath;
+        break;
+    }
+}
+
 require_once __DIR__ . '/mailer.php';
 require_once __DIR__ . '/email-templates.php';
 require_once __DIR__ . '/diplomas.php';
